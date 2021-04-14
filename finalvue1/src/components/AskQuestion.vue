f5ff2655a3344db6f0391ba312125dac1564b349
HeadCode-K <kchu901910@gmail.com>
2021-04-14 15:34:57 +0800

    edit the css of the Button and Side chart

--- finalvue1/src/components/AskQuestion.vue
+++ finalvue1/src/components/AskQuestion.vue
@@ -27,22 +27,15 @@ export default {
 #ask{
   margin: auto;
   width: 50%;
-  font-size:22px;
+  font-size:27px;
   box-shadow: 10px 10px 10px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
   /*background: #81D454;*/
   /*border: 1px;*/
   /*border-style: solid;*/
   /*border-color: #81D454;*/
-}
-br {
-  position: relative;
-  margin-top: 1rem;
-  margin-bottom: 5px;
-  color: #68a076;
-  font-size: 25px;
-  font-weight: bold;
+  color: #eff1f1;
   font-style: italic;
-  background: linear-gradient(to top, rgba(69, 198, 230, 0.58) 10.2%, rgba(131, 237, 203, 0.59) 23.8%, #fff 50.7%);
-}
+  font-weight: bolder;
 
+}
 </style>
