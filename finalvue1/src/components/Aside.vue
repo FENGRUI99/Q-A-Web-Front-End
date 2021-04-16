7c3c62759a512136edf94fc116c1dfe5195ba4a7
cheng-fengrui <1522787278@qq.com>
2021-04-16 16:24:25 +0800

    login

--- finalvue1/src/components/Aside.vue
+++ finalvue1/src/components/Aside.vue
@@ -4,11 +4,11 @@
     <AskQuestion></AskQuestion>
     <br>
     <TopContributors></TopContributors>
-    <dSkeletoniv style="position:fixed;width:100px;height:30px;align:right;top:90%;left:10%">
+    <table style="position:fixed;width:100px;height:30px;align:right;top:90%;left:10%">
       <a href="#top">
         <el-button title="Click back to the top" type="success" class="butt" icon="el-icon-top" circle></el-button>
       </a>
-    </dSkeletoniv>
+    </table>
   </div>
 </template>
 
