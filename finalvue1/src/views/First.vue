dfd7ea6bfdd09a3353dd15e4c340943917bc14da
cheng_fengrui <66202585+FengruiCheng@users.noreply.github.com>
2021-05-15 18:18:43 +0800

    chat --cfr

--- finalvue1/src/views/First.vue
+++ finalvue1/src/views/First.vue
@@ -21,20 +21,49 @@ export default {
   components: {
     'header123': RealHead
   },
-  getHomepage () {
-    this.$router.push({
-      path: '/First/homepage'
+  created () {
+    this.userId = sessionStorage.getItem('user_id')
+    this.userName = JSON.parse(sessionStorage.getItem('user_info')).user_name
+    this.axios.post('http://localhost:8080/getRecentChat', {
+      request: sessionStorage.getItem('user_id')
+    }).then((response) => {
+      this.$store.commit('setUserChatMembers', response.data.entity)
+    }).catch((response) => {
+      console.log(response)
     })
+    let that = this
+    if ('WebSocket' in window) {
+      that.ws = new WebSocket(`ws://localhost:8080/websocket/${this.userId}`)
+      that.$global.setWs(that.ws)
+      that.$global.ws.onopen = function () {
+        console.log('socket连接成功')
+      }
+      that.$global.ws.onerror = function () {
+        console.log('错误')
+      }
+      that.$global.ws.onmessage = function (res) {
+        let data = JSON.parse(res.data)
+        const msgObj = {
+          'date': data.date,
+          'text': { 'text': data.text },
+          'mine': false,
+          'name': data.name,
+          'img': '../image/three.jpeg'
+        }
+        that.$store.commit('addChatList', [data.user_id, msgObj])
+      }
+    } else {
+      // 浏览器不支持 WebSocket
+      console.log('您的浏览器不支持 WebSocket!')
+    }
   },
-  getQuestionDetail () {
-    this.$router.push({
-      path: '/First/ProblemDetailPage/'
-    })
-  },
-  getPersonalDetail () {
-    this.$router.push({
-      path: '/First/PersonalDetail/'
-    })
+  destroyed () {
+    let that = this
+    // 销毁监听
+    that.ws.onclose = function () {
+      // 关闭 websocket
+      console.log('连接已关闭...')
+    }
   }
 }
 </script>
--- finalvue1/src/views/HomePage.vue
+++ finalvue1/src/views/HomePage.vue
@@ -10,7 +10,6 @@
         </el-aside>
         <el-container>
           <el-main>
-            {{this.$store.state.chat_flag}}
             <div v-if="this.$store.state.chat_flag">
               <Chat></Chat>
             </div>
@@ -54,53 +53,7 @@ export default {
   },
   data () {
     return {
-      imgSrc: require('../assets/Image 2021-4-29 at 23.50.jpg'),
-      test: []
-    }
-  },
-  created () {
-    this.userId = sessionStorage.getItem('user_id')
-    this.userName = JSON.parse(sessionStorage.getItem('user_info')).user_name
-    this.axios.post('http://localhost:8080/getRecentChat', {
-      request: sessionStorage.getItem('user_id')
-    }).then((response) => {
-      this.$store.commit('setUserChatMembers', response.data.entity)
-      this.test = response.data.entity
-    }).catch((response) => {
-      console.log(response)
-    })
-    let that = this
-    if ('WebSocket' in window) {
-      that.ws = new WebSocket(`ws://localhost:8080/websocket/${this.userId}`)
-      that.$global.setWs(that.ws)
-      that.$global.ws.onopen = function () {
-        console.log('socket连接成功')
-      }
-      that.$global.ws.onerror = function () {
-        console.log('错误')
-      }
-      that.$global.ws.onmessage = function (res) {
-        let data = JSON.parse(res.data)
-        const msgObj = {
-          'date': data.date,
-          'text': { 'text': data.text },
-          'mine': false,
-          'name': data.name,
-          'img': '../image/three.jpeg'
-        }
-        that.$store.commit('addChatList', [data.user_id, msgObj])
-      }
-    } else {
-      // 浏览器不支持 WebSocket
-      console.log('您的浏览器不支持 WebSocket!')
-    }
-  },
-  destroyed () {
-    let that = this
-    // 销毁监听
-    that.ws.onclose = function () {
-      // 关闭 websocket
-      console.log('连接已关闭...')
+      imgSrc: require('../assets/Image 2021-4-29 at 23.50.jpg')
     }
   }
 }
